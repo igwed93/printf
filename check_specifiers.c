23598c361a34bd8b5a4c98448bff07207b53af6c
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-12 14:49:36 -0700

    Included: check_specifiers.c

---
+++ check_specifiers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check_spec - check that a character is a vlaid specifier
+ * and assigns an appropriate function for its printing.
+ * @format: the specifier (char*)
+ *
+ * Return: pointer to a function, if successful
+ * NULL pointer if not
+ */
+int (*check_spec(const char *format))(va_list)
+{
+	int i;
+
+	func_t my_array[4] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_cent},
+		{NULL, NULL}};
+
+	for (i = 0; my_array[i].t != NULL; i++)
+	{
+		if ((*my_array[i].t) == *format)
+		{
+			return (my_array[i].f);
+		}
+	}
+
+	return (NULL);
+}
