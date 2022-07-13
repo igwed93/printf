29c5517294f9986177cede51995f780590fba468
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-12 17:45:21 -0700

    Handled %d and %i

---
+++ print_int.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_int - function that prints an int to
+ * the stdout
+ * @args: variadic parameter
+ *
+ * Return: number of characters printed to stdout
+ */
+int print_int(va_list args)
+{
+	unsigned int d = va_arg(args, int);
+	int count = 0;
+
+	if (d < 0)
+	{
+		d = -d;
+		count = _putchar('-');
+	}
+
+	if (d == 0)
+	{
+		count = _putchar('0');
+	}
+	count = _puts(transform(d, 10));
+	return (count);
+}
